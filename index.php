b5e47a2dfac6af05f75d52ba20a6a166db34447d
dam1r89 <damir89@gmail.com>
2013-07-10 01:48:16 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+define('PAGES_DIR', 'pages');
+Class SimpleBlog{
+  private $routes = array();
+  private $handlers;
+  public function __construct(){
+    $this->handlers = array(
+        'route' => function($t, $arguments, &$route){
+
+          $newRoute = trim(str_replace(['\'','"'], '', $arguments));
+          $t->routes[$newRoute] = $t->routes[$route];
+          unset($t->routes[$route]);
+          $route = $newRoute;
+        },
+        'title' => function($t, $arguments, $route){
+          $t->routes[$route]['title'] = $arguments;
+        },
+
+
+      );
+    $this->scanPages();
+  }
+
+  public function send404(){
+    header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+    die();
+  }
+
+  private function  scanPages(){
+    $files = scandir(PAGES_DIR);
+    foreach ($files as $file) {
+      if (substr($file,0,1)==='.') continue;
+      $fileContent = file_get_contents(PAGES_DIR.'/'.$file);
+
+      $route = rand();
+      $this->routes[$route] = array('content' => $fileContent);
+
+      foreach ($this->handlers as $property => $handler) {
+
+        preg_match("/\{\{{$property}\s?:\s?(.*)\}\}/i", $fileContent, $matches);
+        if (isset($matches[1])){
+          $handler($this, $matches[1], $route);
+        }
+
+      }
+
+      //clan all
+      $this->routes[$route]['content'] = preg_replace("/{{(.*)}}/i", '', $this->routes[$route]['content']);
+
+    }
+  }
+
+  public function getPage($route){
+    return isset($this->routes[$route]) ? $this->routes[$route] : null;
+  }
+
+}
+
+$sb = new SimpleBlog();
+$r = $_GET['r'];
+$page = $sb->getPage($r);
+if ($page === null) $sb->send404();
+
+$wrapper = file_get_contents('public/index.html');
+
+while(preg_match("/{{(.*)}}/i", $wrapper, $matches)){
+  if ($matches[1]){
+    $property = $matches[1];
+    if (isset($page[$property])){
+      $wrapper = preg_replace("/\{\{{$property}\}\}/i", $page[$property], $wrapper);
+    }
+  }
+}
+
+
+echo $wrapper;
+?>
