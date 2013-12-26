279bac54284c37cf3a5c011e499b5a3e1cccf905
Damir <dam1r89@Damirs-MacBook-Air.local>
2013-12-26 18:51:23 +0100

    Sorting by date

--- app/src/dam1r89/SimpleBlog/pieces/articles.php
+++ app/src/dam1r89/SimpleBlog/pieces/articles.php
@@ -1,7 +1,14 @@
-<?php foreach ($simpleBlog->getPages() as $route => $page): ?>
-<?php if (!isset($page['date'])) continue; ?>
-	<a href="<?php echo $route ?>">
+<?php 
+$pages = array_filter($simpleBlog->getPages(), function($page){ return isset($page['date']); });
+
+usort($pages, function($a, $b){
+	return strtotime($a['date']) >= strtotime($b['date']);
+});
+
+?>
+
+<?php foreach ($pages as $page): ?>
+	<a href="<?php echo $page['route'] ?>">
 		<time datetime="<?php echo $page['date'] ?>"> <?php echo date('d/m/Y',strtotime($page['date'])) ?></time> <?php echo $page['title'] ?>
-	
 	</a>
 <?php endforeach; ?>
